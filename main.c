7fcc8a9230e9cac450b25649b6bd7991fc6df209
HDieTox <tom.compagnon2003@gmail.com>
2024-12-03 09:35:41 +0100

    Main + otherthings

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "bin/utils.h"
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    displayWelcomeMsg();
+    while (1)
+    {
+        /* 
+        gestion du prompt
+        affichage du retour de la fonction 
+        */
+    }
+    
     return EXIT_SUCCESS;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "../bin/constants.h"
 
 void displayWelcomeMsg(){
-    
+
+}
+
+void rep(){
+    char buffer[BUFFER_SIZE];
+
+    read(STDIN_FILENO)
 }
